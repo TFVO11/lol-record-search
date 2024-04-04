a8f85ba8c0e3312659a6bcc4026becf2997fd62e
Kim yeongjun <cosmoyj7733@gmail.com>
2024-04-04 21:11:55 +0900

    feat: add the getAccount

--- src/api/axios/searchUsers.jsx
+++ src/api/axios/searchUsers.jsx
@@ -1,16 +1,32 @@
 import { RIOT_GET } from "./typeOfAPI";
 
 const riotAPI = process.env.REACT_APP_API;
+const riotKey = process.env.REACT_APP_RIOT_KEY;
 const summoner = RIOT_GET.summoner;
 const matchs = RIOT_GET.matchs;
 const masteries = RIOT_GET.masteries;
 const league = RIOT_GET.league;
+const account = RIOT_GET.account;
+
+const headers = {
+  "X-Riot-Token": riotKey
+}
+
+export const getAccountInfo = async (gameNameTag) => {
+  const [gameName, tag] = gameNameTag;
+  try {
+    const res = await axios.get(`${riotAPI}${account}/${gameName}/${tag}`)
+    console.log("getAccount", res);
+  } catch {
+    console.error("계정 정보를 가져오지 못했습니다.", error)
+  }
+} 
 
 export const getSearchUser = async (summonerName) => {
   const Name = summonerName;
 
   try {
-    const res = await axios.get(`${riotAPI}${summoner}/${Name}`);
+    const res = await axios.get(`${riotAPI}${summoner}/${Name}`, headers=headers);
     console.log("getSearchUser", res);
   } catch {
     console.error("소환사 정보를 불러오지 못했습니다.", error);
@@ -18,31 +34,30 @@ export const getSearchUser = async (summonerName) => {
 };
 
 export const getMatches = async (puuid) => {
-  
   try {
-    const res = await axios.get(`${riotAPI}${matchs}/${puuid}/ids`)
+    const res = await axios.get(`${riotAPI}${matchs}/${puuid}/ids`);
     console.log("getMatches", res);
   } catch {
-    console.error("매치 정보를 불러오지 못했습니다.", error)
+    console.error("매치 정보를 불러오지 못했습니다.", error);
   }
-}
+};
 
 export const getMastery = async (encryptedSummonerId) => {
-
   try {
-    const res = await axios.get(`${riotAPI}${masteries}/${encryptedSummonerId}`)
+    const res = await axios.get(
+      `${riotAPI}${masteries}/${encryptedSummonerId}`
+    );
     console.log("getMastry", res);
   } catch {
     console.error("숙련도를 불러오지 못했습니다.", error);
   }
-
-}
+};
 
 export const getLeague = async (encryptedSummonerId) => {
   try {
-    const res = await axios.get(`${riotAPI}${league}/${encryptedSummonerId}`)
-    console.log("getLeague", res)
+    const res = await axios.get(`${riotAPI}${league}/${encryptedSummonerId}`);
+    console.log("getLeague", res);
   } catch {
     console.error("리그 정보를 가져오지 못했습니다.", res);
   }
-}
+};
--- src/api/axios/typeOfAPI.js
+++ src/api/axios/typeOfAPI.js
@@ -1,4 +1,5 @@
 export const RIOT_GET = {
+  account: "/riot/account/v1/accounts/by-riot-id",
   summoner : "/lol/summoner/v4/summoners/by-name",
   matchs : "/lol/match/v5/matches/by-puuid",
   masteries : "/lol/champion-mastery/v4/champion-masteries/by-summoner",
